f45a59cacc119f6d8843e8a264f9546cf08f5705
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-06-17 16:45:42 +0330

    change user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nickname');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('profile');
+            $table->unsignedTinyInteger('type')->default(1);
             $table->timestamps();
         });
     }
