d8579459a056d331afc4fd7e679733883cd493d3
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-06-17 16:45:29 +0330

    create Link model and all needed files

---
+++ database/migrations/2023_06_17_130301_create_links_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->string('title');
+            $table->string('color');
+            $table->string('image');
+            $table->unsignedTinyInteger('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('links');
+    }
+};
